90715acce45b7af065cd337e1b070df43289f6d3
jooojub <jooojub@gmail.com>
2021-11-13 16:44:55 +0900

    Router: Redirect 404 notfound
    Signed-off-by: jooojub <jooojub@gmail.com>

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import PostListPage from './layout/PostListPage'
 import PostPage from './layout/PostPage'
 import SearchPage from './layout/SearchPage'
 
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import "hover.css/css/hover-min.css";
 import 'mdbreact/dist/css/mdb.css';
 import "./style/css/_custom-style.css";
@@ -15,12 +15,14 @@ function App() {
   return (
     <div className="App" style={{overflow: "hidden"}}>
       <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <Route exact path="/" component={MainPage}/>
-        <Route exact path="/profile" component={ProfilePage}/>
-        <Route exact path="/posts/:category" component={PostListPage}/>
-        <Route exact path="/post/:file" component={PostPage}/>
-        <Route exact path="/search/:keyword" component={SearchPage}/>
-
+        <Switch>
+          <Route exact path="/" component={MainPage}/>
+          <Route exact path="/profile" component={ProfilePage}/>
+          <Route exact path="/posts/:category" component={PostListPage}/>
+          <Route exact path="/post/:file" component={PostPage}/>
+          <Route exact path="/search/:keyword" component={SearchPage}/>
+          <Route exact path='*' component={MainPage}/>
+        </Switch>
       </BrowserRouter>
     </div>
   );
